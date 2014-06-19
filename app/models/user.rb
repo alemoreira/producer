6313ca6fcd55e0c5a94865c7e01d8ffc9ec76dc1
Alessandro Moreira <alessandro.moreira@gmail.com>
2014-06-18 23:40:46 -0300

    Adição de validação de presença, unicidade e formato do email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name
+
+  validates :email, :first_name, :last_name, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: 'Not a valid format.'}
 end
