112f795ee24879cc31f7a00096893e41e07181b9
Alessandro Moreira <alessandro.bezerra@detran.ce.gov.br>
2014-06-24 10:09:05 -0300

    populando categorias de post

--- app/models/post/category.rb
+++ app/models/post/category.rb
@@ -1,4 +1,18 @@
 class Post::Category < ActiveRecord::Base
+  RAILS      = 1
+  RUBY       = 2
+  JAVASCRIPT = 3
+  HTML       = 4
+  CSS        = 5
+
   attr_accessible :name
   has_and_belongs_to_many :posts
+
+  def name
+    read_attribute(:name).upcase
+  end
+
+  def name=(value)
+    write_attribute(:name, value.upcase)
+  end
 end
